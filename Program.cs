bb47c8a1da032741512832349d815b49a87c2323
joe mclain <joe.mclainr@outlook.com>
2014-11-15 11:34:41 -0800

    Periodic

--- Program.cs
+++ Program.cs
@@ -33,20 +33,53 @@ namespace FixOneDriveFU {
 
 		/////////////////////////////////////////////////////////////////////////////
 
+		static int totalDirectories = 0;
+		static int totalFiles = 0;
+		static int noBaseFiles = 0;
+
 		static void RecurseAndDisplay( string dirPath )
 		{
-			Write( "directory: {0}", dirPath );
+			++totalDirectories;
+
+			//Write( "directory: {0}", dirPath );
 
 			var dirs = Directory.GetDirectories( dirPath );
 			foreach( var dir in dirs ) {
 				RecurseAndDisplay( dir );
 			}
 
-			Write( "files for {0}", dirPath );
+			//Write( "files for {0}", dirPath );
 
 			var files = Directory.GetFiles( dirPath, "*" + TARGET + ".*" );
 			foreach( var file in files ) {
-				Write( "  {0}", file );
+				++totalFiles;
+				//Write( "  {0}", file );
+				var baseFile = file.Replace( TARGET, "" );
+				if( !File.Exists( baseFile ) ) {
+					++noBaseFiles;
+					Write( "  {0} does not exist", baseFile );
+				}
+			}
+
+		}
+
+
+		/////////////////////////////////////////////////////////////////////////////
+
+		static void FixFiles( string dirPath )
+		{
+			var dirs = Directory.GetDirectories( dirPath );
+			foreach( var dir in dirs ) {
+				FixFiles( dir );
+			}
+
+			var files = Directory.GetFiles( dirPath, "*" + TARGET + ".*" );
+			foreach( var file in files ) {
+				var baseFile = file.Replace( TARGET, "" );
+				if( File.Exists( baseFile ) ) {
+					File.Delete( baseFile );
+					File.Move( file, baseFile );
+				}
 			}
 
 		}
@@ -63,6 +96,15 @@ namespace FixOneDriveFU {
 			//var topLevelFiles = Directory.GetDirectories( Environment.CurrentDirectory );	//, "*{target}.*", SearchOption.AllDirectories );
 
 			RecurseAndDisplay( Environment.CurrentDirectory );
+
+			Write( "total directories {0}", totalDirectories );
+			Write( "total files       {0}", totalFiles );
+			Write( "have no base file {0}", noBaseFiles );
+
+			var key = Console.ReadKey();
+			if( ConsoleKey.Y == key.Key ) {
+				FixFiles( Environment.CurrentDirectory );
+			}
 
 			Exit();
 
